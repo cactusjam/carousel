8d6d10f685c04f12d060f16eab617161ef52149d
Evgenia Alfimova <alfimova.evg@gmail.com>
2021-03-21 23:52:51 +0200

    dell style width for img

--- src/mocks/slides.js
+++ src/mocks/slides.js
@@ -1,34 +1,34 @@
 export default [
   {
     id: 1,
-    content: '<img src="/img/1.jpg" alt="landmark photography of trees near rocky mountain under blue skies daytime" style="width:100%"/>',
+    content: '<img src="/img/1.jpg" alt="landmark photography of trees near rocky mountain under blue skies daytime" />',
   },
   {
     id: 2,
-    content: '<img src="/img/2.jpg" alt="landscape photography of black mountain" style="width:100%"/>',
+    content: '<img src="/img/2.jpg" alt="landscape photography of black mountain" />',
   },
   {
     id: 3,
-    content: '<img src="/img/3.jpg" alt="silhouette of mountains during sunset" style="width:100%"/>',
+    content: '<img src="/img/3.jpg" alt="silhouette of mountains during sunset" />',
   },
   {
     id: 4,
-    content: '<img src="/img/4.jpg" alt="boat docked near house" style="width:100%"/>',
+    content: '<img src="/img/4.jpg" alt="boat docked near house" />',
   },
   {
     id: 5,
-    content: '<img src="/img/5.jpg" alt="empty road between trees on forest" style="width:100%"/>',
+    content: '<img src="/img/5.jpg" alt="empty road between trees on forest" />',
   },
   {
     id: 6,
-    content: '<img src="/img/6.jpg" alt="landscape photography of brown mountain" style="width:100%"/>',
+    content: '<img src="/img/6.jpg" alt="landscape photography of brown mountain" />',
   },
   {
     id: 7,
-    content: '<img src="/img/7.gif" alt="person walks towards trees and mountains" style="width:100%;height:100%"/>',
+    content: '<img src="/img/7.gif" alt="person walks towards trees and mountains" />',
   },
   {
     id: 8,
-    content: '<video style="width:100%" poster="/img/8-poster.jpg" autoplay loop muted ><source src="/img/8.mp4" type="video/mp4"><img src="/img/8-poster.jpg" alt="aerial view of big snowy mountain with pines"></video>',
+    content: '<video  poster="/img/8-poster.jpg" autoplay loop muted ><source src="/img/8.mp4" type="video/mp4"><img src="/img/8-poster.jpg" alt="aerial view of big snowy mountain with pines"></video>',
   }
 ];
